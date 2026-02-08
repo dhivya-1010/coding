4951babcd5d1db15e901a2ce6d4080fc4850d748
DHIVYA V <dhivya.v2024cse@sece.ac.in>
2026-02-08 17:02:26 +0530

    Change bitwise shift to power calculation for 5^n

---
+++ A. Again Twenty Five.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		long res = (long)Math.pow(5,n);
+		System.out.println(res);
+	}
+}
