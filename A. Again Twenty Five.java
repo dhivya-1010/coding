df0cb8d9ca4811a5b83cce0666f7f94475967a2d
DHIVYA V <dhivya.v2024cse@sece.ac.in>
2026-02-09 22:11:30 +0530

    Modify Main to always output 25
    Updated the program to always print 25 regardless of input.

--- A. Again Twenty Five.java
+++ A. Again Twenty Five.java
@@ -1,10 +1,20 @@
-import java.util.*;
-public class Main
-{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		long res = (long)Math.pow(5,n);
-		System.out.println(res);
-	}
+// import java.util.*;
+// public class Main
+// {
+// 	public static void main(String[] args) {
+// 		Scanner sc = new Scanner(System.in);
+// 		int n = sc.nextInt();
+// 		long res = (long)Math.pow(5,n);
+// 		System.out.println(res);
+// 	}
+// }
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();  // read n
+        System.out.println(25);  // answer is always 25
+    }
 }
+
