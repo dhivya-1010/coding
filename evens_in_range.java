01b1dacb56d9079c559676776e4e01a30ba22467
Dhivya V <dhivya.v2024cse@sece.ac.in>
2026-03-08 17:30:23 +0530

    Refactor to optimal prefix sum approach for evens

--- evens_in_range.java
+++ evens_in_range.java
@@ -20,7 +20,7 @@ First argument A is an array of integers.
 Second argument B is a 2D array of integers.
 
 *********************************************************************/
-
+//BRUTE FORCE
 import java.util.*;
 public class Main
 {
@@ -60,3 +60,57 @@ public class Main
         
 	}
 }
+
+//OPTIMAL - PREFIX SUM 
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("size if array A: ");
+		int n = sc.nextInt();
+		
+		int[] arr = new int[n];
+		System.out.print("enter n elements: ");
+		for(int i=0;i<n;i++){
+		    arr[i]=sc.nextInt();
+		}
+		int c = 0;
+		System.out.print("size if array B: ");
+		int m = sc.nextInt();
+		int[][] b = new int[m][2];
+		System.out.print("enter m*2 elements: ");
+		for(int i=0;i<m;i++){
+		    for(int j=0;j<2;j++){
+		        b[i][j] = sc.nextInt();
+		    }
+		}
+		for(int i=0;i<n;i++){
+		    if(arr[i]%2==0){
+		        arr[i]=1;
+		    }else{
+		        arr[i]=0;
+		    }
+		}
+		int[] pf = new int[n];
+		pf[0]=arr[0];
+		for(int i=1;i<n;i++){
+		    pf[i]=pf[i-1]+arr[i];
+		}
+		int[] res = new int[m];
+		for(int i=0;i<m;i++){
+		    int s = b[i][0];
+		    int e = b[i][1];
+		  //  for(int j=s;j<=e;j++){
+		        if(s==0) res[i]=pf[e];
+		        else{
+		            res[i]=pf[e]-pf[s-1];
+		        }
+		    //}
+		    
+		}
+		System.out.println("Ans:"+ Arrays.toString(res));
+        
+	}
+}
+
